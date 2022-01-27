b70e8200b0b379f9d03d7f3a3fe258c8b26095fc
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-27 18:25:35 +0100

    add function prev & next

--- js/script.js
+++ js/script.js
@@ -4,7 +4,24 @@ Vue.config.devtools = true;
 
 const message = new Vue({
     el: '#root',
+
     data: {
-        
-    },
+        currentIndex: 0,
+        images: [
+        'images/image1.jpg',
+        'images/image2.jpg',
+        'images/image3.jpg',
+        'images/image4.jpg',
+    ]},
+
+    // #Function
+
+    methods: {
+        previousImg() {
+            this.currentIndex--
+        },
+        nextImg() {
+            this.currentIndex++
+        },
+    }
 });
